3b9250d293035bed920e8b34f4779950144ddd43
Juan Manuel Gomez <jmgomezc14@gmail.com>
2023-08-17 19:12:40 -0500

    feat: formularios

--- actividades/actividad_2/index.js
+++ actividades/actividad_2/index.js
@@ -5,14 +5,17 @@ const getDataForm = () => {
   const acceptConditions = document.getElementById("acceptConditions").checked;
   const actividadType = document.getElementById("actividadType").value;
   const taskType = document.getElementById("taskType").value;
+  const semestre = document.getElementById("semestre").value;
 
   // Json => clave:valor
   const data = {
     name: txtName,
     email: txtEmail,
+    txtDescription: txtDescription,
     acceptConditions, // sin valor ya que la clave y valor se llaman igual
     actividadType,
     taskType,
+    semestre,
   };
 
   console.log(data);
